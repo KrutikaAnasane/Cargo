cdfdd727f7215bec7cc3f6eae742a0039af60688
KrutikaAnasane <anasaneks15.it@coep.ac.in>
2018-11-23 12:35:54 +0530

    Add files via upload

---
+++ DefaultDB_20180611_111111_Tables.cs
@@ -0,0 +1,86 @@
+﻿using FluentMigrator;
+using System;
+namespace OrderManagementSystem.Migrations.DefaultDB
+{
+    [Migration(20180611111111)]
+    public class DefaultDB_20180611_111111_Tables : Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("orderManagement");
+
+            Create.Table("Region").InSchema("orderManagement")
+               .WithColumn("RegionId").AsInt32().Identity().PrimaryKey().NotNullable()
+               .WithColumn("Description").AsString(200).NotNullable();
+
+            Create.Table("Supplier").InSchema("orderManagement")
+                .WithColumn("SupplierId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Company name").AsString(200).NotNullable()
+                .WithColumn("Contact name").AsString(200).NotNullable()
+                .WithColumn("Contact title").AsString(200).Nullable()
+                .WithColumn("Phone").AsInt64().Nullable()
+                .WithColumn("Region").AsInt32().NotNullable()
+                 .ForeignKey( "FK_SupplierRegion", "orderManagement", "Region", "RegionId");
+
+            Create.Table("Product").InSchema("orderManagement")
+                .WithColumn("ProductId").AsInt32().Identity().PrimaryKey().NotNullable()
+              .WithColumn("Product Name").AsString(200).NotNullable()
+              .WithColumn("Price").AsInt32().Nullable()
+              .WithColumn("SupplierId").AsInt32().NotNullable()
+              .ForeignKey("FK_SupplierProduct", "orderManagement", "Supplier", "SupplierId");
+
+
+            Create.Table("Customer").InSchema("orderManagement")
+                .WithColumn("CustomerId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Customer name").AsString(200).NotNullable()
+                .WithColumn("Contact").AsInt64().Nullable()
+                .WithColumn("City").AsString(200).NotNullable()
+               .WithColumn("Region").AsInt32().NotNullable()
+                 .ForeignKey("FK_CustomerRegion", "orderManagement","Region", "RegionId");
+
+            Create.Table("Shipper").InSchema("orderManagement")
+                .WithColumn("ShipperId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("Company name").AsString(200).NotNullable()
+                .WithColumn("Region").AsInt32().NotNullable()
+                 .ForeignKey("FK_ShipperRegion", "orderManagement", "Region", "RegionId")
+                .WithColumn("Phone").AsInt64().Nullable()
+                .WithColumn("ShippedQuantity").AsInt32().NotNullable();
+
+            Create.Table("Order").InSchema("orderManagement")
+                .WithColumn("OrderId").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("CustomerId").AsInt32().NotNullable()
+                .ForeignKey("FK_CustomerOrder", "orderManagement", "Customer", "CustomerId")
+                .WithColumn("ShipperId").AsInt32().NotNullable()
+                .ForeignKey("FK_ShipperOrder", "orderManagement", "Shipper", "ShipperId")
+                .WithColumn("Order date").AsDateTime().NotNullable()
+                .WithColumn("TrackingId").AsInt32().NotNullable();
+
+            Create.Table("OrderProduct").InSchema("orderManagement")
+           .WithColumn("Id").AsInt32()
+           .Identity().PrimaryKey().NotNullable()
+           .WithColumn("OrderId").AsInt32().NotNullable()
+           .ForeignKey("FK_ProductOrder1", "orderManagement", "Order", "OrderId")
+           .WithColumn("ProductId").AsInt32().NotNullable()
+           .ForeignKey("FK_ProductOrder2", "orderManagement", "Product", "ProductId")
+           .WithColumn("Quantity").AsInt32().NotNullable();
+
+   /*         Execute.Sql(
+           @"INSERT INTO oms.OrderProduct (OrderId, ProductId)
+            SELECT o.OrderId, o.ProductId
+            FROM oms.Order o
+            WHERE o.ProductId IS NOT NULL");       */
+
+            Create.Table("Inventory").InSchema("orderManagement")
+            .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
+            .WithColumn("Date").AsDateTime().NotNullable()
+            .WithColumn("ProductId").AsInt32().NotNullable()
+            .ForeignKey("FK_ProductInventory", "orderManagement", "Product", "ProductId")
+            .WithColumn("Quantity").AsInt32().NotNullable()
+            .WithColumn("Direction").AsInt16().NotNullable();
+        }
+        public override void Down()
+        {
+        }
+    }
+}
+
---
+++ DefaultDB_20180629_111112_AlterInventory.cs
@@ -0,0 +1,49 @@
+﻿using FluentMigrator;
+using System;
+namespace OrderManagementSystem.Migrations.DefaultDB
+{
+    [Migration(20180629111112)]
+    public class DefaultDB_20180629_111112_AlterInventory : Migration
+    {
+        public override void Up()
+        {
+            Delete.Column("ShippedQuantity").FromTable("Shipper").InSchema("orderManagement");
+
+            Create.Table("ShipmentOrder").InSchema("orderManagement")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
+                .WithColumn("ShipperId").AsInt32().NotNullable()
+                .ForeignKey("FK_ShipperShipment", "orderManagement", "Shipper", "ShipperId")
+                .WithColumn("Date").AsDateTime().NotNullable()
+                .WithColumn("ShipmentFrom").AsString(200).Nullable()
+                .WithColumn("ShipmentTo").AsString(200).Nullable()
+                .WithColumn("CustomerId").AsInt32().NotNullable()
+                .ForeignKey("FK_CustomerShipment", "orderManagement", "Customer", "CustomerId")
+                .WithColumn("OrderId").AsInt32().NotNullable()
+                .ForeignKey("FK_OrderShipment", "orderManagement", "Order", "OrderId")
+                .WithColumn("Status").AsInt16().NotNullable();
+
+            Create.Table("ShipmentProduct").InSchema("orderManagement")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
+                 .WithColumn("OrderId").AsInt32().NotNullable()
+                .ForeignKey("FK_OrderShipmentP", "orderManagement", "Order", "OrderId")
+                 .WithColumn("OrderProductId").AsInt32().NotNullable()
+                .ForeignKey("FK_OrderPShipmentP", "orderManagement", "OrderProduct", "Id")
+                .WithColumn("ShippedQuantity").AsInt32().NotNullable();
+
+            Alter.Table("Inventory").InSchema("orderManagement")
+                .AddColumn("SupplierId").AsInt32().Nullable()
+                .ForeignKey("FK_SupplierInventory", "orderManagement", "Supplier", "SupplierId")
+                .AddColumn("OrderId").AsInt32().Nullable()
+                .ForeignKey("FK_OrderInventory", "orderManagement", "Order", "OrderId")
+                .AddColumn("ShipmentId").AsInt32().Nullable()
+                .ForeignKey("FK_InventoryShipment", "orderManagement", "ShipmentOrder", "Id");
+
+
+
+        }
+        public override void Down()
+        {
+        }
+    }
+}
+
